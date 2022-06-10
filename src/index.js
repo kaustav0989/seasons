963d8ed90d6686aa7a0e91947d8a16f144bab987
Kaustav Paul <roni0989@gmail.com>
2022-06-11 01:47:05 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import  ReactDOM  from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const App = () => {
+   
+//     navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//     return (
+//         <div>Latitude : </div>
+//     );
+// }
+
+class App extends React.Component {
+
+    // constructor(props){
+    //     super(props);
+    //     this.state = { lat : null, errorMessage : '' };
+    // }
+
+    state = { lat : null, errorMessage : '' };
+
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat : position.coords.latitude}),
+            err => this.setState({ errorMessage : err.message })
+        );
+    }
+
+    renderContent(){
+        if( this.state.lat && !this.state.errorMessage ){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        if( !this.state.lat && this.state.errorMessage ){
+            return <div>Error : {this.state.errorMessage}</div>
+        }
+        return (
+            <Spinner message="Please allow location request"/>
+        );
+    }
+
+    render(){
+        return (<div>{this.renderContent()}</div>);
+    }
+}
+
+ReactDOM.render(<App /> , document.querySelector('#root'));
